50986026440bba70326edcfb78e58b775de82f53
Dubos <dubos1210@yandex.ru>
2018-06-26 11:46:05 +0300

    Add files via upload

---
+++ Anemometer/Anemometer.ino
@@ -0,0 +1,140 @@
+#define F_CPU 16000000UL //16MHz
+
+#define PLOTTER	0        //Вывод в формате программы SerialPortPlotter
+
+#define rot 2300         //Путь воздуха за один оборот, мм * 10
+
+#define DSOK_flag 0x01
+
+#include <avr/io.h>\
+#include <stdint.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "usart.h"
+#include "DS18B20.h"
+
+int DS_temperature = 0;
+unsigned int windspeed = 0;
+unsigned int counter = 0;
+unsigned char flag = 0;
+
+ISR(TIMER0_COMPA_vect) {
+	counter++;
+	TCNT0 = 0;
+}
+
+ISR(INT1_vect) {
+	EIMSK &=~ (1<<INT0);
+	
+	PORTB |= (1<<5);
+	if(counter > 0) {
+		windspeed = rot / (counter * 2);
+		counter = 0;
+	}
+	
+		#if PLOTTER
+			USART_send(36);
+			if(flag & DSOK_flag) {
+				DS_temperature = DS18B20_temperature();
+				USART_send_decimal(DS_temperature, 1);
+				USART_send(" ");
+			}
+			USART_send_integer((int) windspeed / 10);
+			USART_send(0x2E);
+			USART_send_integer((int) windspeed % 10);
+			USART_send_string(";");
+			USART_send_BK();
+		#else
+		if(windspeed >= 0.001) {
+			if(flag & DSOK_flag) {
+				DS_temperature = DS18B20_temperature();
+				USART_send_string("Temperature: ");
+				USART_send_decimal(DS_temperature, 1);
+				USART_send_string(" C");
+				USART_send_BK();
+			}
+			USART_send_string("Wind speed: ");
+			USART_send_integer((int) windspeed / 10);
+			USART_send(0x2E);
+			USART_send_integer((int) windspeed % 10);
+			USART_send_string(" m/s");
+			USART_send_BK();
+		}
+		#endif
+	
+	_delay_ms(100);	
+	PORTB &=~ (1<<5);
+	
+	EIMSK = 2;
+	EIFR |= (1<<1);
+}
+
+int main(void) {
+	DDRB |= (1<<5); //LED на 13 выводе
+	PORTB |= (1<<5);
+		
+	//USART
+	DDRD &=~ (1<<0);   //PD0 (RX) - выход
+	DDRD |= (1<<1);    //PD1 (TX) - вход
+	USART_init(38400);
+	#if PLOTTER		
+	#else
+		USART_send_string("Dubos Anemometer");
+		USART_send_BK();
+	#endif
+	
+	//Magnet
+	DDRD &=~ (1<<3); //INT1 на 3 ноге
+	PORTD |= (1<<3); //Pullup на INT1 ноге
+	DDRD |= (1<<4);  //Земля на 4 ноге
+	PORTD &=~ (1<<4);
+	
+	EICRA = 8;
+	EIMSK = 2;
+	
+	
+	/*//DS18B20
+	#if PLOTTER
+	#else
+		USART_send_string("Seraching for DS18B20 (Library v0.1 by Dubos)");
+	#endif	
+	if(DS18B20_init() > 0) {
+		#if PLOTTER
+		#else
+			USART_send_string(" - Found");
+			USART_send_BK();
+		#endif	
+		DS18B20_write(SKIP_ROM);
+		DS18B20_write(WRITE_SCRATCHPAD);
+		DS18B20_write(0x7F);
+		DS18B20_write(0x7F);
+		DS18B20_write(0x7F);
+		
+		flag |= (1<<DSOK_flag);
+	}
+	else {
+		#if PLOTTER
+		#else
+			USART_send_string(" - Not found");
+			USART_send_BK();
+		#endif
+		flag &=~ (1<<DSOK_flag);
+	}*/	
+	
+	//For ATmega328
+	TCCR0A = 0;
+	TCCR0B = 3;
+	TIMSK0 = 2;
+	OCR0A = 125;
+		
+	sei();
+	
+	_delay_ms(500);
+	PORTB &=~ (1<<5);
+	
+	while(1) {		
+		
+	}
+	
+	//while(1) {}
+}
---
+++ Anemometer/usart.h
@@ -0,0 +1,100 @@
+/* USART Library for Atmel AVR v0.4
+ *  USART_init(9600);           //Инициализация USART на скорости 9600 бод
+ *  USART_send(0x**);           //Отправка байта 0x**
+ *  USART_send_BK();            //Отправка сигнала "Возврат каретки" (0x0D 0x0A)
+ *  USART_send_string("Hello"); //Отправка строки Hello
+ *  USART_send_integer(123);    //Отправка числа 0123 в виде ASCII-символов (макс. - 9999)
+*/
+
+#ifndef F_CPU
+  #define F_CPU 16000000UL
+#endif
+
+void USART_init(unsigned int baud) {
+  unsigned int myubrr = F_CPU / 16 / baud - 1;
+  UBRR0H = (unsigned char) (myubrr >> 8);
+  UBRR0L = (unsigned char) myubrr;
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  UCSR0C = (0<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00);
+}
+
+void USART_send(char s) {
+  while(!(UCSR0A & (1<<UDRE0))) {};
+  UDR0 = s;
+}
+
+void USART_send_BK(void) {
+  USART_send(0x0D);
+  USART_send(0x0A);
+}
+
+void USART_send_string(const char *data) {
+  unsigned char c;
+  while(( c = *data++ )) {
+    USART_send(c);
+    _delay_ms(10);
+  }
+}
+
+void USART_send_integer(unsigned int c) {
+  c = c % 10000;
+  unsigned char temp = c / 100;
+  if(c >= 1000) { USART_send(0x30 + temp / 10); }
+  if(c >= 100) { USART_send(0x30 + temp % 10); }
+  temp = c % 100;
+  if(c >= 10) { USART_send(0x30 + temp / 10); }
+  USART_send(0x30 + temp % 10);
+}
+
+void USART_send_long(int32_t c) {
+	unsigned long temp = 0;
+	if(c < 0) { USART_send(0x2D); }
+	c %= 10000000000;
+	if(c < 0) { c *= -1;}
+	if(c >= 100000000) {
+		temp = c / 100000000;
+		USART_send(0x30 + temp / 10);
+		USART_send(0x30 + temp % 10);
+	}
+	if(c >= 1000000) {
+		temp = (c % 100000000) / 1000000;
+		USART_send(0x30 + temp / 10);
+		USART_send(0x30 + temp % 10);
+	}
+	if(c >= 10000) {
+		temp = (c % 1000000) / 10000;
+		USART_send(0x30 + temp / 10);
+		USART_send(0x30 + temp % 10);
+	}
+	if(c >= 100) {
+		temp = (c % 10000) / 100;
+		USART_send(0x30 + temp / 10);
+		USART_send(0x30 + temp % 10);
+	}
+	temp = c % 100;
+	USART_send(0x30 + temp / 10);
+	USART_send(0x30 + temp % 10);
+}
+
+void USART_send_decimal(int c, int d) {
+	switch(d) {
+		case 1:
+			USART_send_integer((int) c / 10);
+			USART_send(0x2E);
+			USART_send_integer((int) c % 10);
+			break;
+		case 2:
+			USART_send_integer((int) c / 100);
+			USART_send(0x2E);
+			USART_send_integer((int) c % 100);
+			break;
+		case 3:
+			USART_send_integer((int) c / 1000);
+			USART_send(0x2E);
+			USART_send_integer((int) c % 1000);
+			break;
+		default:
+			USART_send_integer(c);
+			break;
+	}	
+}
